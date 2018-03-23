89d36933a24511d5c635557c5689bac46aa9d91c
mfouda214 <m.fouda@robone.net>
2018-03-23 18:11:40 +0000

    Add items class

---
+++ SplitViewDemo/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  SplitViewDemo
+//
+//  Created by Mohamed Sobhi  Fouda on 3/23/18.
+//  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    var title: String
+    var text: String
+    
+    init(title: String, text: String) {
+        self.title = title
+        self.text = text
+    }
+}
